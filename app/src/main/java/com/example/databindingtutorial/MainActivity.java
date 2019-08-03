f4c4e0832b7946bf70560f40b0a88e7b48855852
Abdul Ahad <abdulahad.momin07@gmail.com>
2019-08-04 01:42:07 +0530

    Added UI

--- app/src/main/java/com/example/databindingtutorial/MainActivity.java
+++ app/src/main/java/com/example/databindingtutorial/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.databindingtutorial;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.databindingtutorial.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    
+    ActivityMainBinding mBinding;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }

b7cbf51fcb93f9388c19091e037c4241ff9be185
Abdul Ahad <abdulahad.momin07@gmail.com>
2019-08-04 19:34:04 +0530

    Added product class with title data binding

--- app/src/main/java/com/example/databindingtutorial/MainActivity.java
+++ app/src/main/java/com/example/databindingtutorial/MainActivity.java
@@ -5,15 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.databindingtutorial.databinding.ActivityMainBinding;
+import com.example.databindingtutorial.model.Product;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
     
     ActivityMainBinding mBinding;
+    Product mProduct;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mProduct = new Product(
+                "Xiaomi Mi A2",
+                "12MP+20MP dual rear camera and 20MP front facing camera",
+                R.drawable.mobile_1,
+                new BigDecimal(17499),
+                new BigDecimal(11999),
+                284,
+                new BigDecimal(4)
+        );
+        mBinding.setProduct(mProduct);
     }
 }
---
+++ app/src/main/java/com/example/databindingtutorial/model/Product.java
@@ -0,0 +1,80 @@
+package com.example.databindingtutorial.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    
+    private String title;
+    private String desc;
+    private int image;
+    private BigDecimal price;
+    private BigDecimal sale_price;
+    private int num_ratings;
+    private BigDecimal ratings;
+    
+    public Product(String title, String desc, int image, BigDecimal price, BigDecimal sale_price, int num_ratings, BigDecimal ratings) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+        this.price = price;
+        this.sale_price = sale_price;
+        this.num_ratings = num_ratings;
+        this.ratings = ratings;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    public void setImage(int image) {
+        this.image = image;
+    }
+    
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    
+    public void setSale_price(BigDecimal sale_price) {
+        this.sale_price = sale_price;
+    }
+    
+    public void setNum_ratings(int num_ratings) {
+        this.num_ratings = num_ratings;
+    }
+    
+    public void setRatings(BigDecimal ratings) {
+        this.ratings = ratings;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDesc() {
+        return desc;
+    }
+    
+    public int getImage() {
+        return image;
+    }
+    
+    public BigDecimal getPrice() {
+        return price;
+    }
+    
+    public BigDecimal getSale_price() {
+        return sale_price;
+    }
+    
+    public int getNum_ratings() {
+        return num_ratings;
+    }
+    
+    public BigDecimal getRatings() {
+        return ratings;
+    }
+}
